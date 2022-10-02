7e3a53a20e3ea7ed30b0cbebd4a5ba6d707fc6ab
Kenny Hung (he) <kennysubs+github@outlook.com>
2022-10-02 22:27:05 +0100

    Ignore backend js files

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello Kenny')
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT
+console.log(port)
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at https://localhost:${port}`)
+})
