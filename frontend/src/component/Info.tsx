e5797e529aa0efa7a71379cf0a48c04f9a2ac85e
Kenny Hung (he) <kennysubs+github@outlook.com>
2022-10-02 23:42:36 +0100

    Wire up front and backend

--- frontend/src/component/Info.tsx
+++ frontend/src/component/Info.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+type Information = {
+  version: string | null
+}
+
+const initialInformation: Information = {
+  version: null
+}
+
 
 function Info() {
+  const [information, setInformation] = useState(initialInformation)
+
+  async function getInformation() {
+    const newInformationResponse = await fetch('/info/')
+    const newInformation = await newInformationResponse.json()
+    console.log(newInformation)
+    setInformation(it => newInformation)
+  }
+  
+  useEffect(() => {
+    getInformation()
+  }, [])
+  // getInformation()
+
   return (
     <div>
       <h1>Information</h1>
-      <p>Info TODO</p>
+      <p>{information.version == null ? 'No version information yet' : `Version: ${information.version}`}</p>
     </div>
   )
 }
