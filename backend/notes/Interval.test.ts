1c678fb4bbbd3fd3aa4ff3b6570b9536388811a0
Kenny Hung (he) <kennysubs+github@outlook.com>
2022-10-12 23:20:52 +0100

    Interval display name for major/perfect

--- backend/notes/Interval.test.ts
+++ backend/notes/Interval.test.ts
@@ -1,19 +1,54 @@
 import Interval from './Interval'
 
+const majorIntervalsMap: { [key: string]: number } = {
+  second: 2,
+  third: 3,
+  sixth: 6,
+  seventh: 7
+}
+
+const perfectIntervalsMap: { [key: string]: number } = {
+  unison: 1,
+  fourth: 4,
+  fifth: 5,
+  octave: 8
+}
+
 describe('Note class', () => {
   describe('major', () => {
-    const ordinals: { [key:string]: number} = {
-      second: 2,
-      third: 3,
-      sixth: 6,
-      seventh: 7
-    }
-    Object.keys(ordinals).forEach(degreeString => {
+    Object.keys(majorIntervalsMap).forEach(degreeString => {
       it(`can instantiate major ${degreeString}`, () => {
-        const degree: number = ordinals[degreeString]
+        const degree = majorIntervalsMap[degreeString]
         const interval = Interval.Major(degree)
-        
+
         expect(interval.displayString).toEqual(`major ${degreeString}`)
+      })
+    })
+    
+    Object.keys(perfectIntervalsMap).forEach(degreeString => {
+      it(`cannot instantiate major ${degreeString}`, () => {
+        const degree = perfectIntervalsMap[degreeString]
+        
+        expect(() => Interval.Major(degree)).toThrow()
+      })
+    })
+  })
+  
+  describe('perfect', () => {
+    Object.keys(perfectIntervalsMap).forEach(degreeString => {
+      it(`can instantiate perfect ${degreeString}`, () => {
+        const degree = perfectIntervalsMap[degreeString]
+        const interval = Interval.Perfect(degree)
+        
+        expect(interval.displayString).toEqual(`perfect ${degreeString}`)
+      })
+    })
+    
+    Object.keys(majorIntervalsMap).forEach(degreeString => {
+      it(`cannot instantiate perfect ${degreeString}`, () => {
+        const degree = majorIntervalsMap[degreeString]
+
+        expect(() => Interval.Perfect(degree)).toThrow()
       })
     })
   })
--- backend/notes/Interval.ts
+++ backend/notes/Interval.ts
@@ -9,28 +9,44 @@ const displayStrings = [
   'octave'
 ]
 
+const perfectDegrees = [1, 4, 5, 8]
+
 class Interval {
   private readonly degree: number
+  private readonly deviation: number
 
   constructor(degree: number) {
     this.degree = degree
+    this.deviation = 0
   }
 
   get displayString() {
-    return `major ${displayStrings[this.degree - 1]}`
+    return `${this.quality} ${displayStrings[this.degree - 1]}`
+  }
+  
+  get quality() {
+    if (this.deviation === 0) {
+      return perfectDegrees.includes(this.degree) ? 'perfect' : 'major'
+    }
   }
 
 }
 
 function Major(degree: number) {
+  if (![2, 3, 6, 7].includes(degree)) {
+    throw `Cannot instantiate major interval of degree ${degree}`
+  }
   return new Interval(degree)
 }
 
-function Minor(degree: number) {
+function Perfect(degree: number) {
+  if (![1, 4, 5, 8].includes(degree)) {
+    throw `Cannot instantiate perfect interval of degree ${degree}`
+  }
   return new Interval(degree)
 }
 
 export default {
   Major,
-  Minor,
+  Perfect,
 }
