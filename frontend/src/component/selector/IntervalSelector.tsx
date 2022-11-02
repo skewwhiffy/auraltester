1c537ef90c1617f50a543d12f640017370b13e3f
Kenny Hung (he) <kennysubs+github@outlook.com>
2022-11-02 20:36:38 +0000

    Interval selector

--- frontend/src/component/selector/IntervalSelector.tsx
+++ frontend/src/component/selector/IntervalSelector.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Col, Container, Form, Row } from 'react-bootstrap'
+import RadioButtons, { RadioButtonDefinition } from '../../util/RadioButtons'
 import ClefSelector from './ClefSelector'
 import NoteSelector from './NoteSelector'
 
@@ -24,6 +25,24 @@ interface State {
 }
 
 class IntervalSelector extends React.Component<Props, State> {
+  private keySignatureValues: Array<RadioButtonDefinition> = [
+    { value: 'C#', label: 'C# / A# minor' },
+    { value: 'F#', label: 'F# / D# minor' },
+    { value: 'B', label: 'B / G# minor' },
+    { value: 'E', label: 'E / C# minor' },
+    { value: 'A', label: 'A / F# minor' },
+    { value: 'D', label: 'D / B minor' },
+    { value: 'G', label: 'G / E minor' },
+    { value: 'C', label: 'None / C / A minor' },
+    { value: 'F', label: 'F / D minor' },
+    { value: 'Bb', label: 'Bb / G minor' },
+    { value: 'Eb', label: 'Eb / C minor' },
+    { value: 'Ab', label: 'Ab / F minor' },
+    { value: 'Db', label: 'Db / Bb minor' },
+    { value: 'Gb', label: 'Gb / Eb minor' },
+    { value: 'Cb', label: 'Cb / Ab minor' },
+  ]
+
   constructor(props: Props) {
     super(props);
     this.state = {
@@ -31,7 +50,7 @@ class IntervalSelector extends React.Component<Props, State> {
       bottomNote: 'C',
       intervalQuality: '',
       intervalSize: '',
-      keySignature: ''
+      keySignature: 'C'
     }
   }
 
@@ -41,24 +60,62 @@ class IntervalSelector extends React.Component<Props, State> {
         <Form>
           <Row>
             <Col>
-              <ClefSelector 
-                defaultValue={this.state.clef} 
-                onChange={clef => this.onFormChange({ ...this.state, clef})}
+              <ClefSelector
+                defaultValue={this.state.clef}
+                onChange={clef => this.onFormChange({ ...this.state, clef })}
               />
             </Col>
             <Col>
               <NoteSelector
                 defaultValue={this.state.bottomNote}
                 includeDoubleAccidentals={true}
-                onChange={bottomNote => this.onFormChange({...this.state, bottomNote})}
+                onChange={bottomNote => this.onFormChange({ ...this.state, bottomNote })}
               />
+            </Col>
+            <Col>
+              <Container>
+                <Row><Col>Interval</Col></Row>
+                <Row>
+                  <Col>
+                    <RadioButtons
+                      values={['diminished', 'minor', 'major', 'perfect', 'augmented'].map(value => ({ value }))}
+                      name='intervalQuality'
+                      defaultValue={this.state.intervalQuality}
+                      onChange={intervalQuality => this.onFormChange({ ...this.state, intervalQuality })}
+                    />
+                  </Col>
+                  <Col>
+                    <RadioButtons
+                      values={Array.from(Array(8).keys()).map(it => it + 1).map(it => ({ value: `${it}` }))}
+                      name='intervalQuality'
+                      defaultValue={this.state.intervalQuality}
+                      onChange={intervalQuality => this.onFormChange({ ...this.state, intervalQuality })}
+                    />
+                  </Col>
+                </Row>
+              </Container>
+            </Col>
+            <Col>
+              <Container>
+                <Row><Col>Key signature</Col></Row>
+                <Row>
+                  <Col>
+                    <RadioButtons
+                      values={this.keySignatureValues}
+                      name='keySignature'
+                      defaultValue={this.state.keySignature}
+                      onChange={keySignature => this.onFormChange({...this.state, keySignature})}
+                    />
+                  </Col>
+                </Row>
+              </Container>
             </Col>
           </Row>
         </Form>
       </Container>
     )
   }
-  
+
   private onFormChange = (newState: State) => {
     this.setState(newState)
     this.onStateChange(newState)
