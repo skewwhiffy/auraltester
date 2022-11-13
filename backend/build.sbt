9a0c23bae76f4c0f6da9e7826321a204c6fcf2ce
Kenny Hung (he) <kennysubs+github@outlook.com>
2022-11-13 21:50:47 +0000

    Info controller, working

--- backend/build.sbt
+++ backend/build.sbt
@@ -11,6 +11,6 @@ libraryDependencies ++= Seq(
   guice,
   "org.mockito" %% "mockito-scala-scalatest" % "1.17.12" % Test,
   "org.scalatest" %% "scalatest" % "3.2.14" % Test,
-  "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+  "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
 )
 PlayKeys.devSettings := Seq("play.server.http.port" -> "3001")
--- backend/test/com/skewwhiffy/auraltester/controller/InfoControllerTest.scala
+++ backend/test/com/skewwhiffy/auraltester/controller/InfoControllerTest.scala
@@ -1,22 +1,24 @@
 package com.skewwhiffy.auraltester.controller
 
-/*
-import com.skewwhiffy.auraltester.testutils.MockInstantiation
-import org.mockito.InjectMocks
-import org.scalatest.GivenWhenThen
-import org.scalatest.flatspec.AnyFlatSpec
+import com.skewwhiffy.auraltester.model.InfoResponse
+import org.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.{JsSuccess, Json}
+import play.api.test.Helpers._
+import play.api.test._
 
-class InfoControllerTest extends AnyFlatSpec with MockInstantiation with GivenWhenThen {
-  @InjectMocks
-  private var infoController: InfoController = _
+import scala.util.chaining.scalaUtilChainingOps
 
-  it should "return information" in {
-    When("info is requested")
-    val result = infoController.index()
+class InfoControllerTest extends PlaySpec with MockitoSugar {
+  "/api/info GET" must {
+    "should be valid" in {
+      val controller = new InfoController(stubControllerComponents())
 
-    Then("info is returned")
-    assert(result != null)
+      val actual = controller.index().apply(FakeRequest(GET, "/api/info"))
+
+      status(actual) mustBe OK
+      contentType(actual) mustBe Some("application/json")
+      assert(contentAsJson(actual).pipe { it => Json.format[InfoResponse].reads(it) }.isSuccess)
+    }
   }
 }
-
- */
