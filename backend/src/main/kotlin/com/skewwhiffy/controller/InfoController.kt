839f054f6eb3a3fc364ca54f4814d382c2793180
Kenny Hung (he) <kennysubs+github@outlook.com>
2022-12-22 14:48:56 +0000

    First response class

--- backend/src/main/kotlin/com/skewwhiffy/controller/InfoController.kt
+++ backend/src/main/kotlin/com/skewwhiffy/controller/InfoController.kt
@@ -1,15 +1,14 @@
 package com.skewwhiffy.controller
 
+import com.skewwhiffy.dto.InfoResponse
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class InfoController {
   @RequestMapping("/api/info")
-  fun get(): Any {
-    return mapOf(
-      "version" to "0.0.1-kotlin"
-    )
+  fun get(): InfoResponse {
+    return InfoResponse("0.0.1-kotlin")
   }
 
 }
---
+++ backend/src/main/kotlin/com/skewwhiffy/dto/InfoResponse.kt
@@ -0,0 +1,3 @@
+package com.skewwhiffy.dto
+
+data class InfoResponse(val version: String)
