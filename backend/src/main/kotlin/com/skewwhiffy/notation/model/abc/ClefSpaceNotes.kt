7f56d31c26cc96f3757fc540e83f0af166ac1735
Kenny Hung (he) <kennysubs+github@outlook.com>
2022-12-22 23:07:19 +0000

    All clef notes tests

--- backend/src/main/kotlin/com/skewwhiffy/notation/model/abc/ClefSpaceNotes.kt
+++ backend/src/main/kotlin/com/skewwhiffy/notation/model/abc/ClefSpaceNotes.kt
@@ -4,7 +4,7 @@ import com.skewwhiffy.notation.model.AbsoluteNote
 import com.skewwhiffy.notation.model.Clef
 
 data class ClefSpaceNotes(private val clef: Clef) : ClefNotes {
-  override val notes: List<AbsoluteNote> = (1..4)
+  override val notes: List<AbsoluteNote> = (1..3)
     .fold(listOf(clef.lowLedgerNote.upOne.skipOne)) { soFar, _ -> soFar + soFar.last().skipOne }
     .map { it.withNoteName }
 }
--- backend/src/main/kotlin/com/skewwhiffy/notation/model/abc/ClefUpperLedgerNotes.kt
+++ backend/src/main/kotlin/com/skewwhiffy/notation/model/abc/ClefUpperLedgerNotes.kt
@@ -6,4 +6,5 @@ import com.skewwhiffy.notation.model.Clef
 data class ClefUpperLedgerNotes(private val clef: Clef) : ClefNotes {
   override val notes: List<AbsoluteNote> = (1..5)
     .fold(listOf(clef.highLedgerNote.downOne)) { soFar, _ -> soFar + soFar.last().upOne }
+    .map { it.withNoteName }
 }
