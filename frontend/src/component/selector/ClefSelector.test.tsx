225cc448fd8b0b0bed06e223d09ce9e8e8a2baf8
Kenny Hung <kennysubs+github@outlook.com>
2022-12-26 16:33:38 +0000

    Fix FE tests (#21)

--- frontend/src/component/selector/ClefSelector.test.tsx
+++ frontend/src/component/selector/ClefSelector.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react';
 describe('ClefSelector', () => {
   it('triggers onChange function supplied', () => {
     const toTrigger = ['Alto Clef', 'Bass Clef', 'Tenor Clef', 'Treble Clef']
-    const expected = ['alto', 'bass', 'tenor', 'treble']
+    const expected = ['treble', 'alto', 'bass', 'tenor', 'treble']
     const triggered: Array<String> = []
     render(
       <ClefSelector onChange={it => triggered.push(it)} />
