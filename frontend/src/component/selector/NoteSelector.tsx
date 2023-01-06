d04fe6cf41cd8df2ca85941f362910862a8e8011
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-01-06 22:17:03 +0000

    Note selector -> function

--- frontend/src/component/selector/NoteSelector.tsx
+++ frontend/src/component/selector/NoteSelector.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Col, Container, Row } from "react-bootstrap"
 import RadioButtons, { RadioButtonDefinition } from "../../util/RadioButtons"
 
@@ -15,7 +15,67 @@ interface State {
   accidental: string
 }
 
-class NoteSelector extends React.Component<Props, State> {
+
+const NoteSelector = (props: Props) => {
+  const accidentals = [
+    { value: '#' },
+    { value: '', label: 'natural' },
+    { value: 'b' }
+  ]
+
+  const extractNoteName = (note: string) => {
+    return note.substring(0, 1)
+  }
+
+  const extractAccidental = (note: string) => {
+    return note.substring(1)
+  }
+
+  const onFormChange = (newState: State) => {
+    setState(newState)
+    onStateChange(newState)
+  }
+
+  const onStateChange = (state: State) => {
+    props.onChange(`${state.noteName}${state.accidental}`)
+  }
+
+  const [state, setState] = useState<State>({
+    noteName: extractNoteName(props.defaultValue),
+    accidental: extractAccidental(props.defaultValue)
+  })
+
+  if (props.includeDoubleAccidentals) {
+    accidentals.unshift({ value: 'x', label: 'double sharp' })
+    accidentals.push({ value: 'bb', label: 'double flat' })
+  }
+
+  return (
+    <Container>
+      <Row><Col>Note</Col></Row>
+      <Row>
+        <Col>
+          <RadioButtons
+            values={'ABCDEFG'.split('').map(it => ({ value: it }))}
+            name='note'
+            defaultValue={state.noteName}
+            onChange={note => onFormChange({ ...state, noteName: note })}
+          />
+        </Col>
+        <Col>
+          <RadioButtons
+            values={accidentals}
+            name='accidental'
+            defaultValue={state.accidental}
+            onChange={accidental => onFormChange({ ...state, accidental })}
+          />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+class NoteSelectorOld extends React.Component<Props, State> {
   private accidentals: Array<RadioButtonDefinition>
 
   constructor(props: Props) {
