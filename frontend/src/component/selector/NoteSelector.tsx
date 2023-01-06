e159bcbb1a1a9da20319635afe3d58d109da3ded
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-01-06 22:24:44 +0000

    Fix tests

--- frontend/src/component/selector/NoteSelector.tsx
+++ frontend/src/component/selector/NoteSelector.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
+import { useState } from "react"
 import { Col, Container, Row } from "react-bootstrap"
-import RadioButtons, { RadioButtonDefinition } from "../../util/RadioButtons"
+import RadioButtons  from "../../util/RadioButtons"
 
 type OnChangeHandler = (note: string) => void
 
@@ -73,71 +73,6 @@ const NoteSelector = (props: Props) => {
       </Row>
     </Container>
   )
-}
-
-class NoteSelectorOld extends React.Component<Props, State> {
-  private accidentals: Array<RadioButtonDefinition>
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      noteName: this.extractNoteName(props.defaultValue),
-      accidental: this.extractAccidental(props.defaultValue)
-    }
-    this.accidentals = [
-      { value: '#' },
-      { value: '', label: 'natural' },
-      { value: 'b' }
-    ]
-    if (props.includeDoubleAccidentals) {
-      this.accidentals.unshift({ value: 'x', label: 'double sharp' })
-      this.accidentals.push({ value: 'bb', label: 'double flat' })
-    }
-  }
-
-  render() {
-    return (
-      <Container>
-        <Row><Col>Note</Col></Row>
-        <Row>
-          <Col>
-            <RadioButtons
-              values={'ABCDEFG'.split('').map(it => ({ value: it }))}
-              name='note'
-              defaultValue={this.state.noteName}
-              onChange={note => this.onFormChange({ ...this.state, noteName: note })}
-            />
-          </Col>
-          <Col>
-            <RadioButtons
-              values={this.accidentals}
-              name='accidental'
-              defaultValue={this.state.accidental}
-              onChange={accidental => this.onFormChange({ ...this.state, accidental })}
-            />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
-
-  private extractNoteName = (note: string) => {
-    return note.substring(0, 1)
-  }
-
-  private extractAccidental = (note: string) => {
-    return note.substring(1)
-  }
-
-  private onFormChange = (newState: State) => {
-    this.setState(newState)
-    this.onStateChange(newState)
-  }
-
-  private onStateChange(state: State) {
-    this.props.onChange(`${state.noteName}${state.accidental}`)
-  }
-
 }
 
 export default NoteSelector
