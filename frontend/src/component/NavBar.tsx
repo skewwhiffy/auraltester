8c881caff5215d6947f711c2796c8c19904198bb
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-01-28 11:07:09 +0000

    Slight tidy up in navbar

--- frontend/src/component/NavBar.tsx
+++ frontend/src/component/NavBar.tsx
@@ -29,15 +29,17 @@ const NavBar = (): JSX.Element => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">The Aural Tester</Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Navbar.Brand>
             <Nav.Link
               as={Link}
               to="/"
               eventKey="/home"
               title="Home"
-            >Home</Nav.Link>
+            >The Aural Tester
+            </Nav.Link>
+          </Navbar.Brand>
+          <Nav className="me-auto">
             <Nav.Link
               as={Link}
               to="/clefs"
