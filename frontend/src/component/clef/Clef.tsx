74614929b443da58fc7a551612d62b3ca7464005
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-02-22 21:21:31 +0000

    Tabbed section component helper

--- frontend/src/component/clef/Clef.tsx
+++ frontend/src/component/clef/Clef.tsx
@@ -1,28 +1,12 @@
-import { Container, Nav } from 'react-bootstrap'
-import { Route, Routes } from 'react-router'
-import { Link } from 'react-router-dom'
-import { Example, Notes, Quiz } from '.'
+import { Container, Nav } from "react-bootstrap"
+import { Route, Routes } from "react-router"
+import { Link } from "react-router-dom"
+import { Example, Notes, Quiz } from "."
+import TabbedSections from '../../util/TabbedSections'
 
 const Clef = (): JSX.Element => {
   return (
-    <Container>
-      <Nav variant="tabs" defaultActiveKey="notes">
-        <Nav.Item>
-          <Nav.Link as={Link} to=".">Notes</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link as={Link} to="example">Example</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link as={Link} to="quiz">Quiz</Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <Routes>
-        <Route index element={<Notes />} />
-        <Route path="example" element={<Example />} />
-        <Route path="quiz" element={<Quiz />} />
-      </Routes>
-    </Container>
+    <TabbedSections />
   )
 }
 
