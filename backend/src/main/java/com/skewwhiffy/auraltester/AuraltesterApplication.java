9c5c376091dbae5d0991737ae7d04cc216782b96
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-03-28 22:55:32 +0100

    Working Info DAO

--- backend/src/main/java/com/skewwhiffy/auraltester/AuraltesterApplication.java
+++ backend/src/main/java/com/skewwhiffy/auraltester/AuraltesterApplication.java
@@ -1,14 +1,12 @@
 package com.skewwhiffy.auraltester;
 
-import com.skewwhiffy.auraltester.dto.Customer;
-import com.skewwhiffy.auraltester.repository.CustomerRepository;
+import com.skewwhiffy.auraltester.dao.Info;
+import com.skewwhiffy.auraltester.repository.InfoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -19,32 +17,13 @@ public class AuraltesterApplication {
 		SpringApplication.run(AuraltesterApplication.class);
 	}
 
-	//@Bean
-	public CommandLineRunner demo(CustomerRepository repository) {
+	@Bean
+	public CommandLineRunner demo(InfoRepository repository) {
 		return (args) -> {
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
-
+			repository.save(new Info("0.0.1-java"));
 			log.info("Customers found with findAll():");
 			log.info("-------------------------------");
-			for (Customer customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-			log.info("");
-
-			Customer customer = repository.findById(1L);
-			log.info("Customer found with findById(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
-
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByLastName("Bauer").forEach(bauer -> log.info(bauer.toString()));
-			log.info("");
+			repository.findAll().forEach(it -> log.info(it.toString()));
 		};
 	}
 
