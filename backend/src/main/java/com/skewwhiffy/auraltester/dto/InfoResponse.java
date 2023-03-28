ccd078b9ec6f8b7d15fb305ebdab310001aca092
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-03-28 22:21:06 +0100

    Backend with working JPA

--- backend/src/main/java/com/skewwhiffy/auraltester/dto/InfoResponse.java
+++ backend/src/main/java/com/skewwhiffy/auraltester/dto/InfoResponse.java
@@ -1,13 +1,4 @@
 package com.skewwhiffy.auraltester.dto;
 
-public class InfoResponse {
-    private final String version;
-
-    public InfoResponse(String version) {
-        this.version = version;
-    }
-
-    public String getVersion() {
-        return version;
-    }
+public record InfoResponse(String version) {
 }
