101343a491a826d022328efe7ca5d237cbd4fe1d
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-04-03 17:29:04 +0100

    Connect FE to BE, working

--- frontend/src/component/clef/Quiz.tsx
+++ frontend/src/component/clef/Quiz.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react"
+import { Button } from "react-bootstrap";
+import axios from "axios";
+
+interface State {
+  currentScore: number
+  question?: any
+}
+
 const Quiz = (): JSX.Element => {
+  const [state, setState] = useState<State>({
+    currentScore: 0
+  });
+
+  const getQuestion = async () => {
+    const options = {
+      method: "POST",
+      url: "/api/question",
+      headers: { "Content-Type": "application/json" },
+      data: { type: "CLEF" }
+    };
+    const response = await axios.request(options);
+    const data = response.data
+    console.log(data)
+  }
+
   return (
     <div>
       <h1>Clef Quiz</h1>
+      <p>Current score {state.currentScore}</p>
+      {state.question ? <p>Question</p> : <Button onClick={getQuestion}>New question</Button>}
     </div>
   )
 }
