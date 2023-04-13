97338cb3672c8afbad8ed54fe2b1c8ff7af97264
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-04-13 20:20:36 +0100

    Gradle -> groovy

---
+++ backend/src/test/java/com/skewwhiffy/auraltester/cucumber/InfoSteps.java
@@ -0,0 +1,18 @@
+package com.skewwhiffy.auraltester.cucumber;
+
+/*
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class InfoSteps {
+    @Given("I hit the info endpoint")
+    public void hitInfoEndpoint() {
+        System.out.println("HELLO MUM");
+    }
+
+    @Then("system information is returned")
+    public void systemInformationReturned() {
+        System.out.println("GOODBYE MUM");
+    }
+}
+ */
---
+++ backend/src/test/resources/feature/info.feature
@@ -0,0 +1,5 @@
+Feature: Info endpoint
+
+Scenario: Get info
+  When I hit the info endpoint
+  Then system information is returned
