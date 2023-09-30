5d57f90eac95c55e5f766bcd0872243d93ea43ac
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-09-30 12:54:41 +0100

    Use Jupiter test, not old one

--- backend/src/test/kotlin/com/skewwhiffy/auraltester/model/interval/IntervalQuestionFactoryTest.kt
+++ backend/src/test/kotlin/com/skewwhiffy/auraltester/model/interval/IntervalQuestionFactoryTest.kt
@@ -1,7 +1,7 @@
 package com.skewwhiffy.auraltester.model.interval
 
 import com.skewwhiffy.auraltester.dao.model
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
