0871ce6659bebc6dbd4dc839126823f112933508
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-10-07 13:12:13 +0100

    Some redundant imports removed

--- backend/src/main/kotlin/com/skewwhiffy/auraltester/model/interval/IntervalQuestionFactory.kt
+++ backend/src/main/kotlin/com/skewwhiffy/auraltester/model/interval/IntervalQuestionFactory.kt
@@ -8,35 +8,16 @@ import com.skewwhiffy.auraltester.helper.oneOf
 import com.skewwhiffy.auraltester.model.Question
 import com.skewwhiffy.auraltester.model.QuestionFactory
 import com.skewwhiffy.auraltester.notation.factory.ClefFactory
-import com.skewwhiffy.auraltester.notation.factory.KeyFactory
-import com.skewwhiffy.auraltester.notation.factory.ScaleTypeFactory
 import com.skewwhiffy.auraltester.notation.model.interval.Interval
 import com.skewwhiffy.auraltester.notation.model.note.*
-import com.skewwhiffy.auraltester.service.ScaleService
 import org.springframework.stereotype.Service
 import kotlin.math.absoluteValue
 
 @Service
 class IntervalQuestionFactory(
     private val clefFactory: ClefFactory,
-    private val keyFactory: KeyFactory,
-    private val scaleService: ScaleService,
-    private val scaleTypeFactory: ScaleTypeFactory,
     objectMapper: ObjectMapper
 ) : QuestionFactory<IntervalQuestionDao>(objectMapper) {
-    private val keys by lazy {
-        "C,G,D,A,E,B,F#,C#,F,Bb,Eb,Ab,Db,Gb,Cb"
-            .split(",")
-            .map(keyFactory::getKey)
-            .map { it.relativeMinor }
-    }
-
-    private val possibleScaleTypes by lazy {
-        listOf(
-            scaleTypeFactory.minorHarmonic,
-            scaleTypeFactory.minorMelodic,
-        )
-    }
 
     override fun makeNewQuestion(): Question<IntervalQuestionDao> {
         val clef = oneOf(ClefType.entries).let(clefFactory::get)
