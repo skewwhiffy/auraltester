5911e09b6c46ca289ee964c28764f51601d55100
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-10-14 12:37:41 +0100

    Add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {RouterProvider} from 'react-router-dom'
 import {createBrowserRouter} from 'react-router-dom'
 import {routesConfig} from "./application/routesConfig";
+import './index.css'
 
 export default () => {
   const router = createBrowserRouter(routesConfig);
--- frontend/src/application/TopBar.tsx
+++ frontend/src/application/TopBar.tsx
@@ -1,3 +1,3 @@
 export default () => {
-  return <h1>TOP BAR</h1>
+  return <h1 className="bg-red-300">TOP BAR</h1>
 }
