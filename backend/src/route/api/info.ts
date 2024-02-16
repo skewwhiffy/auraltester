a8f21a5e89765fdee8945c9b4cf332cc33ced5fe
Kenny Hung (he) <kennysubs+github@outlook.com>
2024-02-16 06:45:54 +0000

    Correct info returned

--- backend/src/route/api/info.ts
+++ backend/src/route/api/info.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('HELLO INFO 3')
+    res.send({
+        version: "ts-0.0.1"
+    })
 })
 
 export default router;
